722d62753b70f399d7d42b24b43423e7c8952245
pedroxpaulos <paulos.pedro@gmail.com>
2023-06-29 15:18:24 +0100

    Navbar Fixes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import { Routes, Route } from "react-router-dom";
-import "./App.css";
-import { SignupPage } from "./pages/signup";
-import { LoginPage } from "./pages/login";
-import { HomePage } from "./pages/homepage";
-import { ShowListPage } from "./pages/ShowListPage";
-import { OurNavbar } from "./components/Navbar";
+import { Routes, Route } from 'react-router-dom';
+import './App.css';
+import { SignupPage } from './pages/signup';
+import { LoginPage } from './pages/login';
+import { HomePage } from './pages/homepage';
+import { ShowListPage } from './pages/ShowListPage';
+import { OurNavbar } from './components/Navbar';
+
 function App() {
-  return (
-    <div className="App">
-      <OurNavbar />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} />
-        <Route path="/shows" element={<ShowListPage />} />
-      </Routes>
-    </div>
-  );
+	return (
+		<div className="App">
+			<OurNavbar />
+			<Routes>
+				<Route path="/" element={<HomePage />} />
+				<Route path="/login" element={<LoginPage />} />
+				<Route path="/signup" element={<SignupPage />} />
+				<Route path="/shows" element={<ShowListPage />} />
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
