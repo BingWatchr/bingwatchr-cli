7773f616c7eb3263086e703f7f1f4c0340c0eb62
Fabio Sdringola <fabio.sdringola@gmail.com>
2023-06-29 15:58:37 +0200

    app js route /shows

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { SignupPage } from './pages/signup';
 import { LoginPage } from './pages/login';
 import { HomePage } from './pages/homepage';
+import { ShowListPage } from './pages/ShowListPage';
 
 function App() {
 	return (
@@ -11,6 +12,7 @@ function App() {
 				<Route path="/" element={<HomePage />} />
 				<Route path="/login" element={<LoginPage />} />
 				<Route path="/signup" element={<SignupPage />} />
+				<Route path="/shows" element={<ShowListPage />} />
 			</Routes>
 		</div>
 	);
