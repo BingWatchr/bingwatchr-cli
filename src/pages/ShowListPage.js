1751c96b589ff661f6bcf0502b9c4d67c50cac18
pedroxpaulos <paulos.pedro@gmail.com>
2023-06-29 15:14:29 +0100

    fixes on Navbar

--- src/pages/ShowListPage.js
+++ src/pages/ShowListPage.js
@@ -1,30 +1,31 @@
-import { useState, useEffect } from "react";
-import { AddShow } from "../components/AddShow";
-import { ShowCard } from "../components/ShowCard";
-import axios from "axios";
-const API_URL = "http://localhost:5005";
+import { useState, useEffect } from 'react';
+import { AddShow } from '../components/AddShow';
+import { ShowCard } from '../components/ShowCard';
+import axios from 'axios';
+const API_URL = 'http://localhost:5005';
 
 export const ShowListPage = () => {
-  const [shows, setShow] = useState([]);
+	const [shows, setShow] = useState([]);
 
-  const getAllShows = () => {
-    axios
-      .get(`${API_URL}/api/shows`)
-      .then((response) => setShow(response.data))
-      .catch((error) => console.log(error));
-  };
+	const getAllShows = () => {
+		axios
+			.get(`${API_URL}/api/shows`)
+			.then((response) => setShow(response.data))
+			.catch((error) => console.log(error));
+	};
 
-  // We set this effect will run only once, after the initial render
-  // by setting the empty dependency array - []
-  useEffect(() => {
-    getAllShows();
-  }, []);
-  return (
-    <div className="ShowListPage">
-      <AddShow refreshProjects={getAllShows} />
-      {shows.map((show) => (
-        <ShowCard key={show._id} {...shows} />
-      ))}
-    </div>
-  );
+	// We set this effect will run only once, after the initial render
+	// by setting the empty dependency array - []
+	useEffect(() => {
+		getAllShows();
+	}, []);
+	return (
+		<div className="ShowListPage">
+			<h1>doing something</h1>
+			<AddShow refreshProjects={getAllShows} />
+			{shows.map((show) => (
+				<ShowCard key={show._id} {...shows} />
+			))}
+		</div>
+	);
 };
