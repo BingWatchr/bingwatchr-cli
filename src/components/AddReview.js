fb78dfcbf120c80f5cc4fa21d87c960d653bc920
pedroxpaulos <paulos.pedro@gmail.com>
2023-06-30 11:00:57 +0100

    Add Review function

--- src/components/AddReview.js
+++ src/components/AddReview.js
@@ -1,58 +1,63 @@
-import { useState } from "react";
-import axios from "axios";
+import { useState } from 'react';
+import axios from 'axios';
+import { useContext } from 'react';
+import { AuthContext } from '../context/auth.context';
 
-const API_URL = "http://localhost:5005";
+const API_URL = 'http://localhost:5005';
 
 export const AddReview = (props) => {
-  const [name, setName] = useState("");
-  const [summary, setSummary] = useState("");
+	const { user } = useContext(AuthContext);
+	const [text, setText] = useState('');
+	const [rating, setRating] = useState(8);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const { showId } = props;
-    const requestBody = { name, summary, showId };
-    // Get the token from the localStorage
-    const storedToken = localStorage.getItem("authToken");
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		const { showId } = props;
+		console.log(user);
+		const author = user._id;
+		const requestBody = { author, text, rating, showId };
+		// Get the token from the localStorage
+		const storedToken = localStorage.getItem('authToken');
 
-    axios
-      .post(`${API_URL}/api/reviews`, requestBody, {
-        headers: { Authorization: `Bearer ${storedToken}` },
-      })
-      .then((response) => {
-        setName("");
-        setSummary("");
-        // Invoke the callback function coming through the props
-        // from the ShowDetailsPage, to refresh the show details
-        props.refreshShow();
-      })
-      .catch((e) => {
-        console.log(e);
-      });
-  };
+		axios
+			.post(`${API_URL}/api/reviews`, requestBody, {
+				headers: { Authorization: `Bearer ${storedToken}` },
+			})
+			.then((response) => {
+				setText('');
+				setRating(8);
+				// Invoke the callback function coming through the props
+				// from the ShowDetailsPage, to refresh the show details
+				props.refreshShow();
+			})
+			.catch((e) => {
+				console.log(e);
+			});
+	};
 
-  return (
-    <div className="AddReview">
-      <h3>Add New Review</h3>
+	return (
+		<div className="AddReview">
+			<h3>Add New Review</h3>
 
-      <form onSubmit={handleSubmit}>
-        <label>Title:</label>
-        <input
-          type="text"
-          name="name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
+			<form onSubmit={handleSubmit}>
+				<label>Title:</label>
+				<input
+					type="text"
+					name="text"
+					value={text}
+					onChange={(e) => setText(e.target.value)}
+				/>
 
-        <label>Description:</label>
-        <textarea
-          type="text"
-          name="summary"
-          value={summary}
-          onChange={(e) => setSummary(e.target.value)}
-        />
+				<label>rating:</label>
+				<textarea
+					type="number"
+					name="text"
+					value={rating}
+					onChange={(e) => setRating(e.target.value)}
+				/>
 
-        <button type="submit">Add Review</button>
-      </form>
-    </div>
-  );
+				<button type="submit">Add Review</button>
+			</form>
+		</div>
+	);
 };
