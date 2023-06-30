36fed066832b2026325bce04cbb95b5ae42d1482
pedroxpaulos <paulos.pedro@gmail.com>
2023-06-30 15:37:05 +0100

    Review improvements

--- src/components/ReviewCard.js
+++ src/components/ReviewCard.js
@@ -1,34 +1,43 @@
-import axios from "axios";
-import { Link } from "react-router-dom";
-const API_URL = "http://localhost:5005";
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+const API_URL = 'http://localhost:5005';
 
-export const ReviewCard = ({ author, text, _id }) => {
-  const deleteReview = (id) => {
-    axios
-      .delete(`${API_URL}/api/reviews/${id}`)
-      .then(() => {
-        window.location.reload(false);
-      })
-      .catch((err) => console.log(err));
-  };
+export const ReviewCard = ({
+	_id,
+	author,
+	text,
+	rating,
+	tvShow,
+	createdAt,
+	updatedAt,
+}) => {
+	const deleteReview = (id) => {
+		axios
+			.delete(`${API_URL}/api/reviews/${id}`)
+			.then(() => {
+				window.location.reload(false);
+			})
+			.catch((err) => console.log(err));
+	};
 
-  return (
-    <div className="TaskCard card">
-      <h3>{author}</h3>
-      <h4>Description:</h4>
-      <p>{text}</p>
+	return (
+		<div className="TaskCard card">
+			<h3>Author: {author}</h3>
+			<h4>Description:</h4>
+			<p>{text}</p>
+			<p>{createdAt}</p>
 
-      <button
-        onClick={(e) => {
-          e.preventDefault();
-          deleteReview(_id);
-        }}
-      >
-        Delete Review
-      </button>
-      <Link to={`/reviews/edit/${_id}`}>
-        <button>Edit Review</button>
-      </Link>
-    </div>
-  );
+			<button
+				onClick={(e) => {
+					e.preventDefault();
+					deleteReview(_id);
+				}}
+			>
+				Delete Review
+			</button>
+			<Link to={`/reviews/edit/${_id}`}>
+				<button>Edit Review</button>
+			</Link>
+		</div>
+	);
 };
--- src/pages/ShowDetails.js
+++ src/pages/ShowDetails.js
@@ -1,53 +1,53 @@
-import { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
-import axios from "axios";
-import { useParams } from "react-router-dom";
-import Card from "react-bootstrap/Card";
-import { AddReview } from "./../components/AddReview";
-import { ReviewCard } from "./../components/ReviewCard";
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
+import Card from 'react-bootstrap/Card';
+import { AddReview } from './../components/AddReview';
+import { ReviewCard } from './../components/ReviewCard';
 
-const API_URL = "http://localhost:5005";
+const API_URL = 'http://localhost:5005';
 
 export const ShowDetailsPage = () => {
-  const [show, setShow] = useState(null);
-  const { showId } = useParams();
+	const [show, setShow] = useState(null);
+	const { showId } = useParams();
 
-  const getShow = () => {
-    axios
-      .get(`${API_URL}/api/shows/${showId}`)
-      .then((response) => {
-        const oneShow = response.data;
-        //takes the html identation away from the text.
-        response.data.summary = response.data.summary.replace(/<[^>]*>?/gm, "");        
-        setShow(oneShow);
-      })
-      .catch((error) => console.log(error));
-  };
+	const getShow = () => {
+		axios
+			.get(`${API_URL}/api/shows/${showId}`)
+			.then((response) => {
+				const oneShow = response.data;
+				//takes the html identation away from the text.
+				response.data.summary = response.data.summary.replace(/<[^>]*>?/gm, '');
+				setShow(oneShow);
+			})
+			.catch((error) => console.log(error));
+	};
 
-  useEffect(() => {
-    getShow();
-  }, []);
+	useEffect(() => {
+		getShow();
+	}, []);
 
-  return (
-    <>
-      {show && (
-        <Card className="p-5">
-          <Card.Img
-            className="p-5"
-            variant="top"
-            style={{ width: "14rem" }}
-            src={show.image.medium}
-          />
-          <Card.Title>{show.name}</Card.Title>
-          <Card.Body>{show.summary} </Card.Body>
-        </Card>
-      )}
-      <AddReview refreshProject={getShow} showId={showId} />
+	return (
+		<>
+			{show && (
+				<Card className="p-5">
+					<Card.Img
+						className="p-5"
+						variant="top"
+						style={{ width: '14rem' }}
+						src={show.image.medium}
+					/>
+					<Card.Title>{show.name}</Card.Title>
+					<Card.Body>{show.summary} </Card.Body>
+				</Card>
+			)}
+			<AddReview refreshProject={getShow} showId={showId} />
 
-      {show &&
-        show.reviews.map((review) => (
-          <ReviewCard key={review._id} {...review} />
-        ))}
-    </>
-  );
+			{show &&
+				show.reviews.map((review) => (
+					<ReviewCard key={review._id} {...review} />
+				))}
+		</>
+	);
 };
