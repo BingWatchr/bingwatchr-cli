40b731de658fc3995846a6ef5468be270688b0f4
Fabio Sdringola <fabio.sdringola@gmail.com>
2023-06-30 10:17:43 +0200

    names correction

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
-import { SignupPage } from './pages/signup';
-import { LoginPage } from './pages/login';
-import { HomePage } from './pages/homepage';
+import { SignupPage } from './pages/Signup';
+import { HomePage } from './pages/Homepage';
 import { ShowDetailsPage } from './pages/ShowDetails';
 import { ShowListPage } from './pages/ShowListPage';
 import { OurNavbar } from './components/Navbar';
+import { LoginPage } from './pages/Login';
 
 function App() {
 	return (
