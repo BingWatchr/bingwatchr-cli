722bb9015cfa9912c8e0d265ae529b024fb6b577
Fabio Sdringola <fabio.sdringola@gmail.com>
2023-06-30 10:46:47 +0200

    update

--- src/pages/ShowDetails.js
+++ src/pages/ShowDetails.js
@@ -1,45 +1,54 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-import { useParams } from 'react-router-dom';
-import Card from 'react-bootstrap/Card';
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+import Card from "react-bootstrap/Card";
+import { AddReview } from "./../components/AddReview";
+import { ReviewCard } from "./../components/ReviewCard";
 
-const API_URL = 'http://localhost:5005';
+const API_URL = "http://localhost:5005";
 
 export const ShowDetailsPage = () => {
-	const [foundShow, setShow] = useState(null);
-	const { showId } = useParams();
+  const [show, setShow] = useState(null);
+  const { showId } = useParams();
 
-	const getShow = () => {
-		axios
-			.get(`${API_URL}/api/shows/${showId}`)
-			.then((response) => {
-				const oneShow = response.data;
-				//takes the html identation away from the text.
-				response.data.summary = response.data.summary.replace(/<[^>]*>?/gm, '');
-				console.log(response.data.summary);
-				setShow(oneShow);
-			})
-			.catch((error) => console.log(error));
-	};
+  const getShow = () => {
+    axios
+      .get(`${API_URL}/api/shows/${showId}`)
+      .then((response) => {
+        const oneShow = response.data;
+        //takes the html identation away from the text.
+        response.data.summary = response.data.summary.replace(/<[^>]*>?/gm, "");
+        console.log(response.data.summary);
+        setShow(oneShow);
+      })
+      .catch((error) => console.log(error));
+  };
 
-	useEffect(() => {
-		getShow();
-	}, []);
+  useEffect(() => {
+    getShow();
+  }, []);
 
-	return (
-		<>
-			{foundShow && (
-				<Card className="p-5">
-					<Card.Img
-						className="p-5"
-						variant="top"
-						style={{ width: '14rem' }}
-						src={foundShow.image.medium}
-					/>
-					<Card.Title>{foundShow.name}</Card.Title>
-					<Card.Body>{foundShow.summary} </Card.Body>
-				</Card>
-			)}
-		</>
-	);
+  return (
+    <>
+      {show && (
+        <Card className="p-5">
+          <Card.Img
+            className="p-5"
+            variant="top"
+            style={{ width: "14rem" }}
+            src={show.image.medium}
+          />
+          <Card.Title>{show.name}</Card.Title>
+          <Card.Body>{show.summary} </Card.Body>
+        </Card>
+      )}
+      <AddReview refreshProject={getShow} showId={showId} />
+
+      {show &&
+        show.reviews.map((review) => (
+          <ReviewCard key={review._id} {...review} />
+        ))}
+    </>
+  );
 };
