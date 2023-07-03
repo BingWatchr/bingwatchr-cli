d08592ecca8f5c399117542f7b159e6d1b32cba1
pedroxpaulos <paulos.pedro@gmail.com>
2023-07-03 16:29:03 +0100

    Merging

--- src/components/ShowCard.js
+++ src/components/ShowCard.js
@@ -1,42 +1,44 @@
-import { Link } from "react-router-dom";
-import Card from "react-bootstrap/Card";
-import { StarDisplay } from "./StarDisplay";
+import { Link } from 'react-router-dom';
+import Card from 'react-bootstrap/Card';
+import { StarDisplay } from './StarDisplay';
+import { FavMovie } from './Favorite';
 export const ShowCard = (value) => {
-  const newSummary = value.summary.replace(/<[^>]*>?/gm, "");
+	const newSummary = value.summary.replace(/<[^>]*>?/gm, '');
 
-  return (
-    <>
-      <Card className="d-flex flex-row align-items-center">
-        <div>
-          {value.image ? (
-            <Card.Img
-              className=""
-              variant="top"
-              style={{ width: "14rem" }}
-              src={value.image}
-              alt="no image"
-            />
-          ) : (
-            <Card.Img
-              className=""
-              variant="top"
-              style={{ width: "14rem" }}
-              alt="no image"
-            />
-          )}
-        </div>
+	return (
+		<>
+			<Card className="d-flex flex-row align-items-center">
+				<div>
+					{value.image ? (
+						<Card.Img
+							className=""
+							variant="top"
+							style={{ width: '14rem' }}
+							src={value.image}
+							alt="no image"
+						/>
+					) : (
+						<Card.Img
+							className=""
+							variant="top"
+							style={{ width: '14rem' }}
+							alt="no image"
+						/>
+					)}
+				</div>
 
-        <div className="d-flex flex-column align-items-center">
-          <Link
-            to={`/shows/${value._id}`}
-            style={{ textDecoration: "none", color: "black" }}
-          >
-            <Card.Title>{value.name}</Card.Title>
-          </Link>
-          <StarDisplay value={value.rating.average} />
-          <Card.Body>{newSummary}</Card.Body>
-        </div>
-      </Card>
-    </>
-  );
+				<div className="d-flex flex-column align-items-center">
+					<Link
+						to={`/shows/${value._id}`}
+						style={{ textDecoration: 'none', color: 'black' }}
+					>
+						<Card.Title>{value.name}</Card.Title>
+					</Link>
+					<StarDisplay value={value.rating.average} />
+					<FavMovie value={value} />
+					<Card.Body>{newSummary}</Card.Body>
+				</div>
+			</Card>
+		</>
+	);
 };
