e32f08daabd6161ce173c4deb30a629b85ed913e
Fabio Sdringola <fabio.sdringola@gmail.com>
2023-07-05 17:41:06 +0200

    please pull

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -3,7 +3,6 @@ import { useContext } from 'react';
 
 export const HomePage = () => {
 	const { user } = useContext(AuthContext);
-	console.log(user);
 	return (
 		<div>
 			<h1>Welcome to the world of TV Shows.</h1>
